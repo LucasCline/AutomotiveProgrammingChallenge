f16a90bfb1eea789b69cb34b3553a20985961ed0
Lucas Cline <lucaswcline@gmail.com>
2020-02-08 12:16:16 -0600

    Initial Commit

---
+++ AutomotiveProgrammingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomotiveProgrammingChallenge
+//
+//  Created by Amanda Bloomer  on 2/8/20.
+//  Copyright © 2020 Lucas Cline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
