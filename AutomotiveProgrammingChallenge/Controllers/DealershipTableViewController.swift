b744c703f75faed3bca5bf942794559f1617a84a
Lucas Cline <lucaswcline@gmail.com>
2020-02-08 22:31:17 -0600

    Added basic core data entity

--- AutomotiveProgrammingChallenge/Controllers/DealershipTableViewController.swift
+++ AutomotiveProgrammingChallenge/Controllers/DealershipTableViewController.swift
@@ -10,56 +10,11 @@ import UIKit
 
 class DealershipTableViewController: UIViewController {
     @IBOutlet weak var dealershipTableView: UITableView!
-    
-    var vehicleInfoList: [VehicleInfo] = []
-    var dealerIds: Set<Int> = []
-    var dealershipInfoList: [DealershipInfo] = []
     lazy var delegate = DealershipTableViewDelegate(viewController: self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         dealershipTableView.delegate = delegate
         dealershipTableView.dataSource = delegate
-        
-        loadDealershipData()
-    }
-    
-    private func loadDealershipData() {
-        let networkingManager = NetworkingManager()
-        //get a datasetId
-        networkingManager.getDatasetId { (datasetId) in
-            //get the list of vehicles provided by a given datasetId
-            networkingManager.getVehicleList(datasetId: datasetId) { (datasetId, vehicleIds) in
-                //create a dispatch group to signal when all of the dealership requests have finished
-                let vehicleDispatchGroup = DispatchGroup()
-                //for each vehicle id in the list, request the data for the vehicle
-                vehicleIds.forEach { (vehicleId) in
-                    vehicleDispatchGroup.enter()
-                    networkingManager.getVehicleInfo(datasetId: datasetId, vehicleId: vehicleId) { (datasetId, vehicleInfo) in
-                        vehicleDispatchGroup.leave()
-                        //persist vehicle info
-                        self.vehicleInfoList.append(vehicleInfo)
-                        self.dealerIds.insert(vehicleInfo.dealerId)
-                    }
-                }
-                
-                vehicleDispatchGroup.notify(queue: .main) {
-                    print("finished the vehicle requests")
-                    let dealershipDispatchGroup = DispatchGroup()
-                    self.dealerIds.forEach { (dealerId) in
-                        dealershipDispatchGroup.enter()
-                        networkingManager.getDealershipInfo(datasetId: datasetId, dealerId: dealerId) { (datasetId, dealershipInfo) in
-                            dealershipDispatchGroup.leave()
-                            self.dealershipInfoList.append(dealershipInfo)
-                        }
-                    }
-                    
-                    dealershipDispatchGroup.notify(queue: .main) {
-                        print("finished the dealership requests")
-                        self.dealershipTableView.reloadData()
-                    }
-                }
-            }
-        }
     }
 }
--- AutomotiveProgrammingChallenge/Controllers/Delegates/DealershipTableViewDelegate.swift
+++ AutomotiveProgrammingChallenge/Controllers/Delegates/DealershipTableViewDelegate.swift
@@ -10,25 +10,69 @@ import UIKit
 
 class DealershipTableViewDelegate: NSObject {
     weak var viewController: DealershipTableViewController?
+    var vehicleInfoList: [VehicleInfo] = []
+    var dealerIds: Set<Int> = []
+    var dealershipInfoList: [DealershipInfo] = []
     init(viewController: DealershipTableViewController) {
+        super.init()
         self.viewController = viewController
+        loadDealershipData()
+    }
+    
+    private func loadDealershipData() {
+        let networkingManager = NetworkingManager()
+        //get a datasetId
+        networkingManager.getDatasetId { (datasetId) in
+            //get the list of vehicles provided by a given datasetId
+            networkingManager.getVehicleList(datasetId: datasetId) { (datasetId, vehicleIds) in
+                //create a dispatch group to signal when all of the dealership requests have finished
+                let vehicleDispatchGroup = DispatchGroup()
+                //for each vehicle id in the list, request the data for the vehicle
+                vehicleIds.forEach { (vehicleId) in
+                    vehicleDispatchGroup.enter()
+                    networkingManager.getVehicleInfo(datasetId: datasetId, vehicleId: vehicleId) { (datasetId, vehicleInfo) in
+                        vehicleDispatchGroup.leave()
+                        //persist vehicle info
+                        self.vehicleInfoList.append(vehicleInfo)
+                        self.dealerIds.insert(vehicleInfo.dealerId)
+                    }
+                }
+                
+                vehicleDispatchGroup.notify(queue: .main) {
+                    print("finished the vehicle requests")
+                    let dealershipDispatchGroup = DispatchGroup()
+                    self.dealerIds.forEach { (dealerId) in
+                        dealershipDispatchGroup.enter()
+                        networkingManager.getDealershipInfo(datasetId: datasetId, dealerId: dealerId) { (datasetId, dealershipInfo) in
+                            dealershipDispatchGroup.leave()
+                            self.dealershipInfoList.append(dealershipInfo)
+                        }
+                    }
+                    
+                    dealershipDispatchGroup.notify(queue: .main) {
+                        print("finished the dealership requests")
+                        self.viewController?.dealershipTableView.reloadData()
+                    }
+                }
+            }
+        }
     }
 }
 
 extension DealershipTableViewDelegate: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("hey")
+        
     }
 }
 
 extension DealershipTableViewDelegate: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.viewController!.dealershipInfoList.count
+        return self.dealershipInfoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "\(self.viewController!.dealershipInfoList[indexPath.row].name)"
+        cell.textLabel?.text = "\(self.dealershipInfoList[indexPath.row].name)"
         return cell
     }
 }
