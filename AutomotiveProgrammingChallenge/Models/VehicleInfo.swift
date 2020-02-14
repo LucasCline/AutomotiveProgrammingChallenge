210f164204411d9394ca9bd1044092fb6f36ac7c
Lucas Cline <lucaswcline@gmail.com>
2020-02-14 00:12:17 -0600

    Made array back to optional vehicles in dealer info

--- AutomotiveProgrammingChallenge/Models/VehicleInfo.swift
+++ AutomotiveProgrammingChallenge/Models/VehicleInfo.swift
@@ -33,7 +33,7 @@ struct VehicleInfo: Codable {
 struct DealershipInfo: Codable {
     var id: Int
     var name: String
-    var vehicles: [VehicleInfo] = []
+    var vehicles: [VehicleInfo]?
     enum CodingKeys: String, CodingKey {
         case id = "dealerId"
         case name
